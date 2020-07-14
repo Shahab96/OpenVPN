95494472e4c14fe90acf746a6396d051b2200bda
Shahab Dogar <shahab96@gmail.com>
2020-07-13 23:08:03 -0700

    Update to 1.51.0 and move to tokyo

--- bin/open_vpn.ts
+++ bin/open_vpn.ts
@@ -2,16 +2,11 @@
 import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
 import { OpenVpnStack } from '../lib/open_vpn-stack';
-import { OpenVpnDNS } from '../lib/open_vpn-dns';
 
 const app = new cdk.App();
 const env = {
-  region: 'us-east-1',
+  region: "ap-northeast-1",
 };
-const { instance } = new OpenVpnStack(app, 'OpenVPN', {
-  env,
-});
-new OpenVpnDNS(app, 'OpenVPNDNS', {
-  instance,
+new OpenVpnStack(app, 'OpenVPN', {
   env,
 });
--- lib/open_vpn-stack.ts
+++ lib/open_vpn-stack.ts
@@ -1,5 +1,6 @@
-import * as cdk from '@aws-cdk/core';
-import * as ec2 from '@aws-cdk/aws-ec2';
+import * as cdk from "@aws-cdk/core";
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as route53 from "@aws-cdk/aws-route53";
 
 export class OpenVpnStack extends cdk.Stack {
   public readonly instance: ec2.Instance;
@@ -7,23 +8,23 @@ export class OpenVpnStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, 'OpenVPNVpc', {
+    const vpc = new ec2.Vpc(this, "Vpc", {
       maxAzs: 1,
     });
 
     const machineImage = new ec2.GenericLinuxImage({
-      'us-east-1': 'ami-00a56d4e2bda9d721',
+      "ap-northeast-1": "ami-0b8612cffd3478ccb",
     });
-    const instanceType = ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3_AMD, ec2.InstanceSize.NANO);
-    const keyName = 'OpenVPNKeyPair';
-    const securityGroup = new ec2.SecurityGroup(this, 'OpenVPNSecurityGroup', {
+    const instanceType = ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3_AMD, ec2.InstanceSize.MICRO);
+    const keyName = "OpenVPNKeyPair";
+    const securityGroup = new ec2.SecurityGroup(this, "SecurityGroup", {
       vpc,
     });
 
     securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.udp(1194));
     securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22));
 
-    this.instance = new ec2.Instance(this, 'OpenVPNInstance', {
+    this.instance = new ec2.Instance(this, "Instance", {
       vpc,
       machineImage,
       instanceType,
@@ -32,6 +33,20 @@ export class OpenVpnStack extends cdk.Stack {
       vpcSubnets: {
         subnetType: ec2.SubnetType.PUBLIC,
       },
+    });
+
+    new route53.ARecord(this, "RecordSet", {
+      recordName: "vpn.shahab96.com",
+      zone: route53.PublicHostedZone.fromHostedZoneAttributes(this, "HostedZone", {
+        hostedZoneId: "Z2GCKEQH874TQR",
+        zoneName: "shahab96.com",
+      }),
+      target: {
+        values: [
+          this.instance.instancePublicIp,
+        ],
+      },
+      ttl: cdk.Duration.seconds(60),
     });
   }
 }
